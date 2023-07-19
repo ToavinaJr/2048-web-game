9d79d944034fb2ecadd1ecd21547281459f0ffdc
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-07-19 19:09:33 +0300

    code reset

--- index.js
+++ index.js
@@ -1,161 +1,87 @@
-const COLUMNS = 4
 const ROWS = 4
+const COLUMNS = 4
 
-var test = () => {
-      return "hi"
-} 
-class Position {
-      constructor(x,y) {
-            if (x >= 0 && x < COLUMNS){
+/* *********************************************************
+ ********         P O S I T I O N          *****************
+ ******************************************************** */
+
+class Position{
+      constructor(x, y){
+            if (x >= 0 && x < COLUMNS && y >= 0 && y >= 0 && y < ROWS) {
                   this.x = x
-            }
-            else return
-
-            if (y >= 0 && y < ROWS){
                   this.y = y
             }
-            else return
+            else{
+                  throw("Invalide position")
+                  console.error("Out of bound")
+            }
       }
 
-      get_x = () => {
-            return this.x
-      }
-      get_y = () => {
-            return this.y
-      }
-};
+      get_x = () => { return this.x}  
+      get_y = () => { return this.y}
+}
+
+/* ********************************************************* */
 
 function get_randomPosition(){
-      let x = Math.floor(Math.random() * COLUMNS)
-      let y = Math.floor(Math.random() * ROWS)
-
-      return new Position(x,y)
+      let x = Math.floor(Math.random()*4)
+      let y = Math.floor(Math.random()*4)
+      return new Position(x, y)
 }
 
-function get_randomTilevalue() {
-      let factor = Math.floor(Math.random()*7)
 
-      if (factor == 2 || factor == 5)
-            return 4
-      else return 2
-}
-
-class Board {
-      constructor() {
-            this.isGameOver = false
+/* *********************************************************
+ ********          B  O  A  R  D           *****************
+ ******************************************************** */
+class Board{
+      constructor(){
+            this.emptyCase = 16
       }
 
-      get_gameOver = () => {
-            return this.gameOver
+      get_dataAtPosition = (pos) => {
+            return this.data[ pos.get_x() ] [ pos.get_y() ]
       }
 
-      set_gameOver = function(value) {
-            this.gameOver = value
-      }
+      get_positionEmpty = ( ) => {
+            if (this.emptyCase > 0){
+                  let x = Math.floor(Math.random()*4)
+                  let y = Math.floor(Math.random()*4)
+                  let pos = new Position(x, y)
 
-      get_dataAtPosition = (position) => {
-            return this.data[position.get_x()][position.get_y()]
-      }
-
-      set_dataAtPosition = (position, number) => {
-            this.data[position.get_x()][position.get_y()] = number
-      }
-
-      is_dataZero = (position) => {
-            return (this.get_dataAtPosition(position) == 0)
-      }
-
-      get_htmlTileID = (position) => {
-            return document.getElementById(toString(position.get_x()) + '-' + toString(position.get_y()))
-      }
-
-      set_htmlTileID = (position, value) => {
-            let tile = this.get_htmlTileID(position)
-            tile.textContent = toString(value)
-      }
-
-      set_htmlTileClassStyle = (position, style) => {
-            let tile = this.get_htmlTileID(position)
-            tile.classList.add(text)
-      }
-
-      draw = () => {
-            for (let r=0; r < ROWS; ++r){
-                  for (let c = 0; c < COLUMNS; ++c) {
-                        let div = document.createElement('div')
-                        div.id = r.toString() + '_' + c.toString()
-                        div.textContent = ""
-                        div.classList.value = ""
-                        div.classList.add('tile')
-
-                        if (this.data[r][c] > 0){ 
-                              div.textContent = this.data[r][c]
-                              div.classList.add('sprite-' + this.data[r][c].toString())
-                        }
-                        document.getElementById('board').append(div)
+                  while(this.get_dataAtPosition(pos) != 0){
+                        x = Math.floor(Math.random()*4)
+                        y = Math.floor(Math.random()*4)
+                        pos = new Position(x, y)
                   }
-            }
-      }
 
-      run = () => {
-            this.generator()
-            document.addEventListener('keyup', (e) => {
-                  switch(e.code){
-                        case 'ArrowsLeft':
-                              this.slide('Left')
-                              break;
-
-                        case 'ArrowsRight':
-                              this.slide('Right')
-                              break;
-
-                        case 'ArrowsDown':
-                              this.slide('Down')                        
-                              break;
-                        
-                        case 'ArrowsUp':
-                              this.slide('Up')                        
-                              break;                              
-                  }
-            })
-            this.draw()
-      }
-
-      slide = function(direction) {
-            switch(direction){
-                  case 'Up':
-                        break;
                   
-                  case 'Down':
-                        break;
-
-                  case 'Left':
-                        break;
-                        
-                  case 'Right':
-                        break;
-
+                  return pos
             }
       }
 
-      generator = () => {
-            var pos = get_randomPosition()
-            var value = get_randomTilevalue()
-            
-            while(!this.is_dataZero(pos)){
-                  pos = get_randomPosition()   
-            }
-
-            this.set_dataAtPosition(pos, value)
+      set_dataAtPosition = (pos, value) => {
+            this.data[ pos.get_x() ] [ pos.get_y() ] = value
       }
-
-      data = [
+      
+      data = 
+            [
+                  [0, 0, 0, 0], 
                   [0, 0, 0, 0],
-                  [0, 2, 0, 0],
-                  [0, 0, 8, 4],
+                  [0, 0, 0, 0],
                   [0, 0, 0, 0]
             ]
-};
+}
 
-var game = new Board
-game.run()
+class Render{
+
+}
+
+class Handler{
+
+}
+
+class Game{
+
+}
+
+let pos = new Position(-1, 5)
