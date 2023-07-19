b8f14a8198b88fad32c353754d20562c8f2a96f5
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-07-19 19:29:53 +0300

    Class board + class Position

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const ROWS = 4
 const COLUMNS = 4
 
+/* ********************************************************* */
+
+function removeZero(array) {
+      return array.filter(num => num != 0)
+}
+
+/* ********************************************************* */
+
+function generateValue( ){
+      let value = Math.floor(Math.random() * 8)
+
+      if (value == 2  || value == 5)
+            return 4
+
+      return 2
+}
+
 /* *********************************************************
  ********         P O S I T I O N          *****************
  ******************************************************** */
@@ -13,7 +30,6 @@ class Position{
             }
             else{
                   throw("Invalide position")
-                  console.error("Out of bound")
             }
       }
 
@@ -24,8 +40,9 @@ class Position{
 /* ********************************************************* */
 
 function get_randomPosition(){
-      let x = Math.floor(Math.random()*4)
-      let y = Math.floor(Math.random()*4)
+      let x = Math.floor(Math.random() * 4)
+      let y = Math.floor(Math.random() * 4)
+
       return new Position(x, y)
 }
 
@@ -38,9 +55,19 @@ class Board{
             this.emptyCase = 16
       }
 
+      /* ********************************************************* */
+
+      get_emptycase = () => {
+            return this.emptyCase
+      }
+
+      /* ********************************************************* */
+      
       get_dataAtPosition = (pos) => {
             return this.data[ pos.get_x() ] [ pos.get_y() ]
       }
+
+      /* ********************************************************* */
 
       get_positionEmpty = ( ) => {
             if (this.emptyCase > 0){
@@ -53,16 +80,57 @@ class Board{
                         y = Math.floor(Math.random()*4)
                         pos = new Position(x, y)
                   }
-
                   
                   return pos
             }
       }
 
+      /* ********************************************************* */
+
+      get_column = (column) => {
+            if (column >= 0 && column < COLUMNS){
+                  let result = new Array
+                  for (let r=0; r < ROWS; ++r) {
+                        result[r] = this.data[ r ] [ column ]
+                  }
+                  return result
+            }
+      }
+
+      /* ********************************************************* */
+
+      get_row = (row) => {
+            if (row >= 0 && row < ROWS){
+                  let result = new Array
+                  for (let c=0; c < COLUMNS; ++c) {
+                        result[r] = this.data[ row ] [ c ]
+                  }
+                  return result
+            }
+      }
+
+      /* ********************************************************* */
+
       set_dataAtPosition = (pos, value) => {
+            if (this.get_dataAtPosition(pos) == 0)
+                  this.emptyCase--
+
             this.data[ pos.get_x() ] [ pos.get_y() ] = value
       }
-      
+
+      /* ********************************************************* */
+
+      generator = () => {
+            if (this.emptyCase > 0){
+                  let pos = this.get_positionEmpty()
+                  let value = generateValue()
+
+                  this.set_dataAtPosition(pos, value)
+            }
+      }
+
+      /* ********************************************************* */
+
       data = 
             [
                   [0, 0, 0, 0], 
