5d931d96e2a59a9f9b59ad4a901c22ce1d64bb62
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-07-19 14:56:13 +0300

    feat slide()

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const COLUMNS = 4
 const ROWS = 4
 
-class Position{
-      constructor(x,y){
+var test = () => {
+      return "hi"
+} 
+class Position {
+      constructor(x,y) {
             if (x >= 0 && x < COLUMNS){
                   this.x = x
             }
@@ -14,13 +17,12 @@ class Position{
             else return
       }
 
-      get_x = function(){
+      get_x = () => {
             return this.x
       }
-      get_y = function(){
+      get_y = () => {
             return this.y
       }
-
 };
 
 function get_randomPosition(){
@@ -43,7 +45,7 @@ class Board {
             this.isGameOver = false
       }
 
-      get_gameOver = function() {
+      get_gameOver = () => {
             return this.gameOver
       }
 
@@ -51,41 +53,109 @@ class Board {
             this.gameOver = value
       }
 
-      get_dataAtPosition = function(position) {
+      get_dataAtPosition = (position) => {
             return this.data[position.get_x()][position.get_y()]
       }
 
-      set_dataAtPosition = function(position, number) {
+      set_dataAtPosition = (position, number) => {
             this.data[position.get_x()][position.get_y()] = number
       }
 
-      is_dataZero = function(position) {
+      is_dataZero = (position) => {
             return (this.get_dataAtPosition(position) == 0)
       }
 
-      get_htmlTileID = function(position) {
+      get_htmlTileID = (position) => {
             return document.getElementById(toString(position.get_x()) + '-' + toString(position.get_y()))
       }
 
-      set_htmlTileID = function(position, value) {
+      set_htmlTileID = (position, value) => {
             let tile = this.get_htmlTileID(position)
             tile.textContent = toString(value)
       }
 
-      set_htmlTileClassStyle = function(position, style) {
+      set_htmlTileClassStyle = (position, style) => {
             let tile = this.get_htmlTileID(position)
             tile.classList.add(text)
       }
 
+      draw = () => {
+            for (let r=0; r < ROWS; ++r){
+                  for (let c = 0; c < COLUMNS; ++c) {
+                        let div = document.createElement('div')
+                        div.id = r.toString() + '_' + c.toString()
+                        div.textContent = ""
+                        div.classList.value = ""
+                        div.classList.add('tile')
+
+                        if (this.data[r][c] > 0){ 
+                              div.textContent = this.data[r][c]
+                              div.classList.add('sprite-' + this.data[r][c].toString())
+                        }
+                        document.getElementById('board').append(div)
+                  }
+            }
+      }
+
+      run = () => {
+            this.generator()
+            document.addEventListener('keyup', (e) => {
+                  switch(e.code){
+                        case 'ArrowsLeft':
+                              this.slide('Left')
+                              break;
+
+                        case 'ArrowsRight':
+                              this.slide('Right')
+                              break;
+
+                        case 'ArrowsDown':
+                              this.slide('Down')                        
+                              break;
+                        
+                        case 'ArrowsUp':
+                              this.slide('Up')                        
+                              break;                              
+                  }
+            })
+            this.draw()
+      }
+
+      slide = function(direction) {
+            switch(direction){
+                  case 'Up':
+                        break;
+                  
+                  case 'Down':
+                        break;
+
+                  case 'Left':
+                        break;
+                        
+                  case 'Right':
+                        break;
+
+            }
+      }
+
+      generator = () => {
+            var pos = get_randomPosition()
+            var value = get_randomTilevalue()
+            
+            while(!this.is_dataZero(pos)){
+                  pos = get_randomPosition()   
+            }
+
+            this.set_dataAtPosition(pos, value)
+      }
+
       data = [
-                  [7, 0, 0, 0],
                   [0, 0, 0, 0],
-                  [0, 0, 0, 0],
+                  [0, 2, 0, 0],
+                  [0, 0, 8, 4],
                   [0, 0, 0, 0]
             ]
 };
 
-let p = get_randomPosition()
-let board = new Board()
-
-console.log(p.get_x(), p.get_y(), get_randomTilevalue(), board.is_dataZero(p))
+var game = new Board
+game.run()
